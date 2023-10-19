4428b8fbe32ad1789d063c0ab11e1fe4b6acfa96
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-10-18 23:16:51 -0700

    Complete?

--- exercise_three/main.cpp
+++ exercise_three/main.cpp
@@ -1,27 +1,60 @@
 #include <iostream>
 #include <string>
-#include"HashMap.h"
+#include <cmath>
+#include <fstream>
+#include <unordered_map>
 
 //Hashcodes
 class HashCode {
 public:
-    int operator() (const char* k, int len) {
+    //polynomial hash code
+    int operator() (const std::string& s, const int a = 37) {
         unsigned int h = 0;
-        
-        for(int i = 0; i < len; i++) {
-            h = (h << 5) | (h >> 27);
-            h += (unsigned int) k[i];
+        int k = s.size();
+
+        for(int i = 0; i < k; i++) {
+            int power = (k - 1) - i;
+            h += (unsigned int) s[i] * std::pow(a, power);
         }
 
-        return operator() (int(h));
+        return h;
     }
 
 };
 
 int main(int argc, char const *argv[])
 {
-    HashMap<int, std::string, HashCode<std::string>> map;
 
+    std::cout << "Author: Jesus Rodriguez-Luna" << std::endl;
+
+    std::ifstream US_DoI_File("C:/Users/jesus/Documents/GitHub/Hash-Map/usdeclarPC.txt");
+
+    //not open?
+    if(!US_DoI_File.is_open()) std::cout << "Not Open!" << std::endl;
+
+
+    HashCode h;
+    std::unordered_map<int, int> map;
+    std::string word = "";
+
+    while(US_DoI_File >> word) {
+        int hasher = std::abs(h(word, 0));
+
+        if(map.find(hasher) != map.end()) {
+            map[hasher]++;
+        }
+        else {
+            map[hasher] = 1;
+        }
+    }
+
+    auto p = map.begin();
+    std::cout << "{(HASH, COLLISION)}, a = 0" << std::endl;
+    while(!(p == map.end())) {
+        std::cout << "{(" << p->first << ", " << p->second << ")}" << std::endl;
+        ++p;
+    }
+    
     
     return 0;
 }
