7b2efa3d7ee84cfe2e6f7549cc28c483cf43b1f2
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-10-19 20:14:13 -0700

    Check if correct

--- ex_cred/ec_main.cpp
+++ ex_cred/ec_main.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <fstream>
 #include <unordered_map>
+#include <cctype>
 
 //Hashcodes
 class HashCode {
@@ -20,15 +21,23 @@ public:
     }
 };
 
+std::string lowerCase(std::string& s) {
+    std::string word = s;
+    for(char& c : s){
+        c = std::tolower(c);
+    }
+
+    return s;
+}
+
 int main(int argc, char const *argv[])
 {
-
     std::cout << "Author: Jesus Rodriguez-Luna" << std::endl;
     std::unordered_map<int, int> collisions;
     std::unordered_map<int, std::string> wordList;
     std::ifstream US_DoI_File("C:/Users/jesus/GitHub/Hash-Map/usdeclarPC.txt");
     HashCode h;
-    int k = 0;
+    int hasher = 0;
     std::string word = "";
     int sumOfCollisions = 0;
 
@@ -36,28 +45,28 @@ int main(int argc, char const *argv[])
     if(!US_DoI_File.is_open()) std::cout << "Not Open!" << std::endl;
 
     while(US_DoI_File >> word) {
-        k = std::abs(h(word, 40));
-
-        //collisions
-        if(collisions.find(k) != collisions.end()){
-            collisions[k]++;
+        //before generating hashcode, check if word exists
+        hasher = std::abs(h(lowerCase(word)));
+        auto p = wordList.find(hasher);
+        if(p == wordList.end()){
+            wordList[hasher] = word;
+            collisions[hasher] = 1;
         }
         else {
-            collisions[k] = 1;
+            collisions[hasher]++;
         }
-
-        //words repeated ignore
-        wordList[k] = word;
     }
 
-    //n collisions
-    auto iter = collisions.begin();
-    while(iter != collisions.end()){
-        if(iter->second > 1) {
-            sumOfCollisions += iter->second;
-        }
+    auto q = collisions.begin();
+    while(q != collisions.end()) {
+        if(q->second > 1) sumOfCollisions += q->second;
+        ++q;
+    }
 
-        ++iter;
+    auto p = wordList.begin();
+    while(p != wordList.end()) {
+        std::cout << "{" << p->first << ", " << p->second << "}" << std::endl;
+        ++p;
     }
 
     std::cout << "Cyclic Shift Hashcode - Number of collisions: " << sumOfCollisions << std::endl;
